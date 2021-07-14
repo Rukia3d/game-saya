249d9bfa627fc6bba1fd9c4daa7278fdf2279df1
Inga Pflaumer <warda.elentari@gmail.com>
2021-07-14 21:46:47 +1000

    Added Big Card Test

---
+++ src/__test__/BigCard.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BigCard } from "../Fight/BigCard";
+import userEvent from "@testing-library/user-event";
+
+const card = {
+  id: "base_hit1_maya",
+  name: "Maya Hit 1",
+  strength: 1,
+  quantity: 1,
+  character: "maya",
+  element: "earth",
+  image: "../img/Spells/spell1.jpg",
+  selected: true,
+  owner: "hero" as "hero",
+};
+
+test("Renders Big Card screen", async () => {
+  const setInfo = jest.fn();
+  render(<BigCard card={card} setInfo={setInfo} />);
+  expect(screen.getByText("Maya Hit 1")).toBeInTheDocument();
+  expect(screen.getByText("Info")).toBeInTheDocument();
+  userEvent.click(screen.getByText("Info"));
+  expect(setInfo.mock.calls.length).toBe(1);
+});
